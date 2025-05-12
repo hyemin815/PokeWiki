2a2ac941559267167b8a6d7b698c58cc50a9cc2d
hyemin <hmpark15@gmail.com>
2025-05-12 16:46:37 +0900

    Initial Commit

---
+++ PokeWiki/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokeWiki
+//
+//  Created by 박혜민 on 5/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
