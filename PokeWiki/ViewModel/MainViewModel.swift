0fb9528a3a92c61c88a3567e6abe4c44ccede9d1
hyemin <hmpark15@gmail.com>
2025-05-15 14:56:28 +0900

    fix: MainViewModel 외 불필요한 코드 제거

--- PokeWiki/ViewModel/MainViewModel.swift
+++ PokeWiki/ViewModel/MainViewModel.swift
@@ -4,20 +4,15 @@ import Foundation
 import RxSwift
 
 class MainViewModel {
-
+    
     private var disposeBag = DisposeBag()
     
-    // view가 구독할 수 있도록 subject 생성
+    // view가 구독할 수 있도록 subject 생성, 초기값은 PokemonList 타입을 요소로 갖는 빈 배열
     let pokemonListSubject = BehaviorSubject(value: [PokemonList]())
-    let pokemonDetailSubject = BehaviorSubject(value: [PokemonDetailResponse]() )
-    
-    // 임의값 지정
-    var pokemonID = 1
     
     // viewModel이 생성되자마자 데이터를 자동으로 불러오기 위해 init에서 함수 호출
     init() {
         fetchPokemonList()
-        fetchPokemonDetail()
     }
     
     // viewModel에서 수행해야할 비즈니스 로직
@@ -31,24 +26,10 @@ class MainViewModel {
         // NetworkManager의 fetch 메서드 구독
         NetworkManager.shared.fetch(url: url)
             .subscribe(onSuccess: { [weak self] (response: PokemonListResponse) in
+                // PokemonListResponse의 results 값을 방출
                 self?.pokemonListSubject.onNext(response.results)
             }, onFailure: { [weak self] error in
                 self?.pokemonListSubject.onError(error)
             }).disposed(by: disposeBag)
-    }
-    
-    func fetchPokemonDetail() {
-        guard let url = URL(string: "https://pokeapi.co/api/v2/pokemon/\(pokemonID)/") else {
-            pokemonDetailSubject.onError(NetworkError.invalidUrl)
-            return
-        }
-        
-        NetworkManager.shared.fetch(url: url)
-            .subscribe(onSuccess: { [weak self] (response: PokemonDetailResponse) in
-                self?.pokemonDetailSubject.onNext(response)
-            }, onFailure: { [weak self] error in
-                self?.pokemonDetailSubject.onError(error)
-            }).disposed(by: disposeBag)
-        
     }
 }
